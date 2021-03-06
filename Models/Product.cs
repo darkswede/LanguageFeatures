b2d5bb70a9537dbced52b6fa4c12e09e33920996
Marcin Pietrucha <mpietrucha@g2a.com>
2021-03-06 19:55:36 +0100

    basic view added

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProduct()
+        {
+            Product kayak = new Product
+            {
+                Name = "kayak",
+                Price = 275M
+            };
+
+            Product lifejacket = new Product
+            {
+                Name = "lifejacket",
+                Price = 48.95M
+            };
+
+            return new Product[] {kayak, lifejacket, null};
+        }
+    }
+}
